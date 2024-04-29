4cbfd382d2462e4f3d1f3e594071d24354a88431
ramonpablon <ramonpablon@outlook.com>
2024-04-29 13:52:38 -0300

    game commit

---
+++ Rudolf/Assets/Gizmos/CanvasController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class CanvasController : MonoBehaviour
+{
+    public static CanvasController Instance;
+
+    Animator anim;
+
+    float time = 0;
+
+    public TextMeshProUGUI speedText;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        speedText = GetComponentInChildren<TextMeshProUGUI>();
+        anim.Play("On");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerController.Instance.move == 0)
+        {
+            time += Time.deltaTime;
+        }
+
+        if (PlayerController.Instance.move != 0)
+        {
+            time = 0;
+        }
+
+        if (time > 2)
+        {
+            anim.Play("On");
+        }
+        if (time == 0)
+        {
+            anim.Play("Off");
+        }
+
+
+        if (GameManager.Instance.isTimelinePart2)
+        {
+            anim.Play("CanvasTimeline2");
+        }
+    }
+
+    void GoToTimeline2()
+    {
+        GameManager.Instance.isTimelinePart2 = true;
+    }
+
+    public void ShowSpeedText()
+    {
+        speedText.text = "Velocidade " + PlayerController.Instance.speedMul.ToString("0") + "X";
+        anim.Play("speedAmount", 4, 0f);
+    }
+
+
+
+
+    public void OpenNews31()
+    {
+        Application.OpenURL("https://sway.office.com/LJFbxW0nHiRjBK1W?ref=Link");
+    }
+    public void OpenNews36()
+    {
+        Application.OpenURL("https://sway.office.com/IsdmCuLLd1vn7Fg3?ref=Link");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Rudolf/Assets/Resources/Scripts/CamTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CamTrigger : MonoBehaviour
+{
+    public int triggMode;
+    public float lerpSpeed;
+
+    public CinemachineVirtualCamera vcam;
+
+    private CinemachineTransposer transposer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transposer = vcam.GetCinemachineComponent<CinemachineTransposer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D trigg)
+    {
+        if (trigg.gameObject.tag.Equals("Player"))
+        {
+            switch (triggMode)
+            {
+                case 0:
+                    print("inBossScene");
+                    transposer.m_FollowOffset = Vector3.Lerp(transposer.m_FollowOffset, new Vector3(7, 6, -30), Time.deltaTime * lerpSpeed);
+                    break;
+                case 1:
+                    print("inGameScene");
+                    transposer.m_FollowOffset = Vector3.Lerp(transposer.m_FollowOffset, new Vector3(-4, 6, -30), Time.deltaTime * lerpSpeed);
+                    break;
+                case 2:
+                    print("transitionToWife");
+                    transposer.m_FollowOffset = Vector3.Lerp(transposer.m_FollowOffset, new Vector3(-5.8f, 6, -30), Time.deltaTime * lerpSpeed);
+                    break;
+                case 3:
+                    print("transitionToDog");
+                    transposer.m_FollowOffset = Vector3.Lerp(transposer.m_FollowOffset, new Vector3(9f, 6, -30), Time.deltaTime * lerpSpeed);
+                    break;
+            }
+        }
+    }
+}
